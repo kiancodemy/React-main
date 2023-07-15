88cb61e67a25a843e4a28ba89bc89e8b04c68e5b
kian kheirani <kiankheirani@gmail.com>
2023-07-15 13:53:22 +0430

    'add-state'

--- src/component/newexpense/expenseform.js
+++ src/component/newexpense/expenseform.js
@@ -1,20 +1,39 @@
 import "./expenseform.css";
+import { useState } from "react";
 export default function Expenseform() {
+  const [entertitle, settitle] = useState(" ");
+  const [enteramount, setamount] = useState(" ");
+  const [enterdate, setdate] = useState(" ");
+  const titlechange = (e) => {
+    settitle(e.taget.value);
+  };
+  const mountcahange = (e) => {
+    setamount(e.taget.value);
+  };
+  const datachange = (e) => {
+    setdate(e.taget.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls ">
         <div className="new-expense__control ">
           <label>Title</label>
-          <input type="text"></input>
+          <input onChange={titlechange} type="text"></input>
         </div>
 
         <div className="new-expense__control ">
           <label>Amount</label>
-          <input type="number" min={0.1}></input>
+          <input onChange={mountcahange} type="number" min={0.1}></input>
         </div>
         <div className="new-expense__control ">
           <label>Date</label>
-          <input type="date" min={"2019-05-06"} max={"2025-05-06"}></input>
+          <input
+            onChange={datachange}
+            type="date"
+            min={"2019-05-06"}
+            max={"2025-05-06"}
+          ></input>
         </div>
       </div>
       <div className="new-expense__actions ">
